95ba9a891c533893bf7209ba3c7afbf137a743ba
MichaelRodionov <106465054+MichaelRodionov@users.noreply.github.com>
2023-01-21 22:30:50 +0300

    add flake8

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from os import path
 # ----------------------------------------------------------------
 # configuration class
 class Config:
-
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///' \
+                              f'{path.abspath(path.join("database", "posts.db"))}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_PRETTYPRINT_REGULAR = True
     JSON_AS_ASCII = False
